5305ebfdb1e6926de8b0a9e25d46d4e6ccc746da
Jann Horn <jannhorn@googlemail.com>
2011-12-11 20:22:53 +0100

    add webserver

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ var audionode = require('./')
   , AudioReader = audionode.AudioReader
   , AudioLeecher = audionode.AudioLeecher
   , net = require('net')
+  , http = require('http')
+  , url = require('url')
 
 var rooms = {}
+var webserver = http.createServer(function(req, res) {
+  var requrl = url.parse(req.url)
+  var urlparts = requrl.pathname.split('/')
+  if (urlparts[0] !== '' || urlparts[1] !== 'listen' || !urlparts[2]) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    return res.end('invalid url')
+  }
+  var roomName = urlparts[2]
+  if (!rooms.hasOwnProperty(roomName) || !rooms[roomName].ready) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    return res.end('room name is unused\n')
+  }
+  res.writeHead(200, {'Content-Type': 'audio/x-wav'})
+  new AudioLeecher({socket: res, audioReader: rooms[roomName], attachDelay: 10000})
+})
+webserver.listen(8001)
+
 var server = net.createServer(function(c) {
   var header = new Buffer(0)
   c.on('data', headerDataListener)
